a93a05f9894102c26e90007372d1a1b463257b31
jakob <jakobnonnenmacher@hotmail.com>
2022-12-05 14:01:34 +0100

    feedback API working

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
 //Products
 const APIresponse = (param) => {
   for (i = 0; i < param.products.length; i++) {
-      console.log(i);
-      console.log(param.products.length);
-      console.log(param.products[i]);
-      console.log(param.products);
-      document.getElementById("products").innerHTML +=
-          ' <div class="product-row">\
+    console.log(i);
+    console.log(param.products.length);
+    console.log(param.products[i]);
+    console.log(param.products);
+    document.getElementById("products").innerHTML +=
+      ' <div class="product-row">\
                 <div class="flip-box">\
                     <div class="flip-box-inner">\
                             <div class="flip-box-front">\
-                                <h2>'+ param.products[i].category.name +'</h2><br>\
-                                <p>'+param.products[i].sku+'</p><br>\
-                                <p>'+param.products[i].name+'</p><br>\
-                                <p>'+param.products[i].price+'</p><br><br>\
-                                <p>'+param.products[i].category.name+'</p>\
+                                <h2>' +
+      param.products[i].category.name +
+      "</h2><br>\
+                                <p>" +
+      param.products[i].sku +
+      "</p><br>\
+                                <p>" +
+      param.products[i].name +
+      "</p><br>\
+                                <p>" +
+      param.products[i].price +
+      "</p><br><br>\
+                                <p>" +
+      param.products[i].category.name +
+      '</p>\
                             </div>\
                             <div class="flip-box-back">\
                                 <h2>Beschreibung</h2>\
-                                <p>'+param.products[i].description+'</p>\
-                                <img src="'+param.products[i].image+'" alt="" wide="50%" height="50%">\
+                                <p>' +
+      param.products[i].description +
+      '</p>\
+                                <img src="' +
+      param.products[i].image +
+      '" alt="" wide="50%" height="50%">\
                             </div>\
                     </div>\
                 </div>\
@@ -29,75 +43,78 @@ const APIresponse = (param) => {
 
 fetch("https://web-modules.dev/api/v1/products", {
   headers: {
-      Authorization: "Bearer 87|Li56CUyS4RmeOywFWwM3Gmq0YKw0nYvBPQygTxgx", // Part nach "Bearer " mit eigenem Token ersetzen
+    Authorization: "Bearer 87|Li56CUyS4RmeOywFWwM3Gmq0YKw0nYvBPQygTxgx", // Part nach "Bearer " mit eigenem Token ersetzen
   },
 })
   .then((result) => result.json()) // JSON aus der Antwort annehmen
   .then((json) => APIresponse(json))
   .catch(
-      (error) => console.error(error)
-      // Fehlerbehandlung
+    (error) => console.error(error)
+    // Fehlerbehandlung
   );
 
 //Testimonials
 const APITestimonials = (param) => {
   for (i = 0; i < param.testimonials.length; i++) {
-      console.log(i);
-      console.log(param.testimonials.length);
-      console.log(param.testimonials[i]);
-      console.log(param.testimonials[i].lastname);
-      console.log(param.testimonials);
-      document.getElementById("testimonials").innerHTML +=
-          '<div class="testimonial-col">\
+    console.log(i);
+    console.log(param.testimonials.length);
+    console.log(param.testimonials[i]);
+    console.log(param.testimonials[i].lastname);
+    console.log(param.testimonials);
+    document.getElementById("testimonials").innerHTML +=
+      '<div class="testimonial-col">\
              <div class="testimonial">\
                 <div class="testimonial-card">\
                     <img src="' +
-          param.testimonials[i].avatar +
-          '" alt="">\
+      param.testimonials[i].avatar +
+      '" alt="">\
                     <div class="testimonial-title"> <div class="name">' +
-          param.testimonials[i].lastname +
-          " " +
-          param.testimonials[i].firstname +
-          '</div>\
+      param.testimonials[i].lastname +
+      " " +
+      param.testimonials[i].firstname +
+      '</div>\
                     <div class="company">' +
-          param.testimonials[i].company +
-          '</div>\
+      param.testimonials[i].company +
+      '</div>\
                     </div>\
                 </div>\
                 <div class="testimonial-quote">' +
-          param.testimonials[i].quote +
-          '</div>\
+      param.testimonials[i].quote +
+      '</div>\
                 <div class="testimonial-rating">Likes: <button type="button" onClick="like(\'testimonial\',' +
-          param.testimonials[i].id +
-          ')">' +
-          param.testimonials[i].likes_count +
-          '</button>\
+      param.testimonials[i].id +
+      ')">' +
+      param.testimonials[i].likes_count +
+      "</button>\
         </div>\
             </div>\
-        </div>';
+        </div>";
   }
 };
 //get Abfrage
 fetch("https://web-modules.dev/api/v1/testimonials/10", {
   headers: {
-      Authorization: "Bearer 87|Li56CUyS4RmeOywFWwM3Gmq0YKw0nYvBPQygTxgx", // Part nach "Bearer " mit eigenem Token ersetzen
+    Authorization: "Bearer 87|Li56CUyS4RmeOywFWwM3Gmq0YKw0nYvBPQygTxgx", // Part nach "Bearer " mit eigenem Token ersetzen
   },
 })
   .then((result) => result.json()) // JSON aus der Antwort annehmen
   .then((json) => APITestimonials(json))
   .catch(
-      (error) => console.error(error)
-      // Fehlerbehandlung
+    (error) => console.error(error)
+    // Fehlerbehandlung
   );
 //Post
 const like = (type, id) =>
   console.log(
-      "https://web-modules.dev/api/v1/like?type=" + type + "&id=" + id + ""
+    "https://web-modules.dev/api/v1/like?type=" + type + "&id=" + id + ""
   );
 fetch("https://web-modules.dev/api/v1/like?type=" + type + "&id=" + id + "", {
   method: "POST",
   mode: "no-cors",
   headers: {
-      Authorization: "Bearer 87|Li56CUyS4RmeOywFWwM3Gmq0YKw0nYvBPQygTxgx", // Part nach "Bearer " mit eigenem Token ersetzen
+    Authorization: "Bearer 87|Li56CUyS4RmeOywFWwM3Gmq0YKw0nYvBPQygTxgx", // Part nach "Bearer " mit eigenem Token ersetzen
   },
-});
+  body: "type=testimonials&id=3",
+})
+  .then((result) => result.json())
+  .then((json) => console.log(json));
--- js/validateFormular.js
+++ js/validateFormular.js
@@ -4,140 +4,173 @@ const eMail = document.getElementById("email");
 const ratingDesign = document.getElementById("rating-design");
 const ratingComponents = document.getElementById("rating-components");
 const comment = document.getElementById("kommentar");
-const ratingStarsDesign = [...document.getElementsByClassName("rating-star-design")];
-const ratingStarsComponents = [...document.getElementsByClassName("rating-star-components")];
+const ratingStarsDesign = [
+  ...document.getElementsByClassName("rating-star-design"),
+];
+const ratingStarsComponents = [
+  ...document.getElementsByClassName("rating-star-components"),
+];
 const emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 const numbers = /^[0-9]+$/;
 let ratingDesignCount = 0;
 let ratingComponentsCount = 0;
 let error = false;
 
+async function postFeedback() {
+  const rawResponse = await fetch(
+    "https://web-modules.dev/api/v1/feedback?name=" +
+      lastName.value +
+      "&email=" +
+      eMail.value +
+      "&rating_design=" +
+      ratingDesignCount +
+      "&rating_components=" +
+      ratingComponentsCount +
+      "&comment=" +
+      comment.value +
+      "",
+    {
+      method: "POST",
+      headers: {
+        Authorization: "Bearer 88|CgKZ24rOo76Go9RiyzmOUzb3v1qu8sfVtWXrFGp5",
+      },
+    }
+  );
+  const content = await rawResponse.json();
 
-const postFeedback = () => {
-    fetch ("https://web-modules.dev/api/v1/feedback",{
-        method: "POST",
-        headers: {
-            Authorization: "Bearer 87|Li56CUyS4RmeOywFWwM3Gmq0YKw0nYvBPQygTxgx",
-        },
-        body: 'name='+lastName+'&email='+eMail+'&rating_design='+ratingDesignCount+'&rating_components='+ratingComponentsCount+'&comment='+comment+''
-    });
+  console.log(content.amount);
 }
 
 function validate() {
-
-    if (validateText(firstName) | validateText(lastName) | validateEmail(eMail) | validateRating(ratingDesign, ratingDesignCount) | validateRating(ratingComponents, ratingComponentsCount)){
-        error = true;
-    }
-    console.log(error);
-    if (error == false) {
-        //Send somewhere;
-    }
+  if (
+    validateText(firstName) |
+    validateText(lastName) |
+    validateEmail(eMail) |
+    validateRating(ratingDesign, ratingDesignCount) |
+    validateRating(ratingComponents, ratingComponentsCount)
+  ) {
+    error = true;
+  }
+  //console.log(error);
+  if (error == false) {
+    //Send somewhere;
+  }
 }
 
-function validateText(textField){
-    if (textField.value.match(numbers)){
-        let parent = textField.parentElement;
-        parent.querySelector(".error").innerText = "Ungültige Zeichen";
-        parent.querySelector(".error").style.display = "block";
-        return true
-    }
-    if (textField.value.length == 0 || textField.value.length < 3) {
-        let parent = textField.parentElement;
-        parent.querySelector(".error").innerText = "Mindestens 3 Zeichen";
-        parent.querySelector(".error").style.display = "block";
-        return true
-    }
-    if (textField.value.length > 100) {
-        let parent = textField.parentElement;
-        parent.querySelector(".error").innerText = "Maximal 100 Zeichen";
-        parent.querySelector(".error").style.display = "block";
-        return true
-    }
-
+function validateText(textField) {
+  if (textField.value.match(numbers)) {
     let parent = textField.parentElement;
     parent.querySelector(".error").style.display = "none";
     return false;
-}
+  }
 
-function validateEmail(textField){
+  function validateEmail(textField) {
     if (textField.value.length == 0) {
-        let parent = textField.parentElement;
-        parent.querySelector(".error").innerText = "Erforderlich";
-        parent.querySelector(".error").style.display = "block";
-        return true
-    }
-    else if(!textField.value.match(emailFormat)) {
-        let parent = textField.parentElement;
-        parent.querySelector(".error").innerText = "Kein gültiges Format";
-        parent.querySelector(".error").style.display = "block";
-        return true;
-    }
-    else if (textField.value.length > 200) {
-        let parent = textField.parentElement;
-        parent.querySelector(".error").innerText = "Maximal 200 Zeichen";
-        parent.querySelector(".error").style.display = "block";
-        return true
-    }else{
-
-        //FormularPOST
-        postFeedback();
-
+      let parent = textField.parentElement;
+      parent.querySelector(".error").innerText = "Erforderlich";
+      parent.querySelector(".error").style.display = "block";
+      return true;
+    } else if (!textField.value.match(emailFormat)) {
+      let parent = textField.parentElement;
+      parent.querySelector(".error").innerText = "Kein gültiges Format";
+      parent.querySelector(".error").style.display = "block";
+      return true;
+    } else if (textField.value.length > 200) {
+      let parent = textField.parentElement;
+      parent.querySelector(".error").innerText = "Maximal 200 Zeichen";
+      parent.querySelector(".error").style.display = "block";
+      return true;
+    } else {
+      //FormularPOST
+      postFeedback();
     }
 
     let parent = textField.parentElement;
-    parent.querySelector(".error").style.display = "none";
-    return false;
+    parent.querySelector(".error").innerText = "Mindestens 3 Zeichen";
+    parent.querySelector(".error").style.display = "block";
+    return true;
+  }
+  if (textField.value.length > 100) {
+    let parent = textField.parentElement;
+    parent.querySelector(".error").innerText = "Maximal 100 Zeichen";
+    parent.querySelector(".error").style.display = "block";
+    return true;
+  }
 
-
+  let parent = textField.parentElement;
+  parent.querySelector(".error").style.display = "none";
+  return false;
 }
 
-function validateRating(starRating, starRatingCount){
-    if (starRatingCount == 0) {
-        let parent = starRating.parentElement;
-        parent.querySelector(".error").innerText = "Wenigstens einen Stern haben wir verdient...";
-        parent.querySelector(".error").style.display = "block";
-        return true
-    }
+function validateEmail(textField) {
+  if (textField.value.length == 0) {
+    let parent = textField.parentElement;
+    parent.querySelector(".error").innerText = "Erforderlich";
+    parent.querySelector(".error").style.display = "block";
+    return true;
+  } else if (!textField.value.match(emailFormat)) {
+    let parent = textField.parentElement;
+    parent.querySelector(".error").innerText = "Kein gültiges Format";
+    parent.querySelector(".error").style.display = "block";
+    return true;
+  } else if (textField.value.length > 200) {
+    let parent = textField.parentElement;
+    parent.querySelector(".error").innerText = "Maximal 200 Zeichen";
+    parent.querySelector(".error").style.display = "block";
+    return true;
+  } else {
+    //FormularPOST
 
+    postFeedback();
+  }
+
+  let parent = textField.parentElement;
+  parent.querySelector(".error").style.display = "none";
+  return false;
+}
+
+function validateRating(starRating, starRatingCount) {
+  if (starRatingCount == 0) {
     let parent = starRating.parentElement;
-    parent.querySelector(".error").style.display = "none";
-    return false;
+    parent.querySelector(".error").innerText =
+      "Wenigstens einen Stern haben wir verdient...";
+    parent.querySelector(".error").style.display = "block";
+    return true;
+  }
+
+  let parent = starRating.parentElement;
+  parent.querySelector(".error").style.display = "none";
+  return false;
 }
 
 function executeRating(stars, starPrefix) {
-    const starClassActive = starPrefix + "fa-solid fa-star"
-    const starClassUnactive = starPrefix + "fa-regular fa-star";
-    const starsLength = stars.length;
-    let i;
-    let currentRating = 0;
-    stars.map((star) => {
-        star.onclick = () => {
-            i = stars.indexOf(star);
-            writeResult(stars, i+1);
-            if (star.className.indexOf(starClassUnactive) !== -1) {
-                for (i; i >= 0; --i) stars[i].className = starClassActive;
-            } else {
-                for (i; i < starsLength; ++i) stars[i].className = starClassUnactive;
-            }
+  const starClassActive = starPrefix + "fa-solid fa-star";
+  const starClassUnactive = starPrefix + "fa-regular fa-star";
+  const starsLength = stars.length;
+  let i;
+  let currentRating = 0;
+  stars.map((star) => {
+    star.onclick = () => {
+      i = stars.indexOf(star);
+      writeResult(stars, i + 1);
+      if (star.className.indexOf(starClassUnactive) !== -1) {
+        for (i; i >= 0; --i) stars[i].className = starClassActive;
+      } else {
+        for (i; i < starsLength; ++i) stars[i].className = starClassUnactive;
+      }
 
-            star.className = starClassActive
-
-        };
-    });
-
+      star.className = starClassActive;
+    };
+  });
 }
 
-function writeResult(ratingType, rating){
-    if (ratingType == ratingStarsDesign){
-        ratingDesignCount = rating;
-    }
-    else if (ratingType == ratingStarsComponents){
-        ratingComponentsCount = rating;
-    }
+function writeResult(ratingType, rating) {
+  if (ratingType == ratingStarsDesign) {
+    ratingDesignCount = rating;
+  } else if (ratingType == ratingStarsComponents) {
+    ratingComponentsCount = rating;
+  }
 }
 
-
-executeRating(ratingStarsDesign,"rating-star-design ");
-executeRating(ratingStarsComponents,"rating-star-components ");
-
-
+executeRating(ratingStarsDesign, "rating-star-design ");
+executeRating(ratingStarsComponents, "rating-star-components ");
