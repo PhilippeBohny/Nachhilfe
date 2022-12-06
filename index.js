95179b5ff6a7e98f65265fdb590ac20d1720fcc0
Philippe Bohny <philippe.bohny@students.ffhs.ch>
2022-12-06 21:20:15 +0100

    Es wurden nicht alle Dateien committet

--- index.js
+++ index.js
@@ -1,39 +1,25 @@
 //Products
 const APIresponse = (param) => {
   for (i = 0; i < param.products.length; i++) {
-    console.log(i);
-    console.log(param.products.length);
-    console.log(param.products[i]);
-    console.log(param.products);
-    document.getElementById("products").innerHTML +=
-      ' <div class="product-row">\
+      console.log(i);
+      console.log(param.products.length);
+      console.log(param.products[i]);
+      console.log(param.products);
+      document.getElementById("products").innerHTML +=
+          ' <div class="product-row">\
                 <div class="flip-box">\
                     <div class="flip-box-inner">\
                             <div class="flip-box-front">\
-                                <h2>' +
-      param.products[i].category.name +
-      "</h2><br>\
-                                <p>" +
-      param.products[i].sku +
-      "</p><br>\
-                                <p>" +
-      param.products[i].name +
-      "</p><br>\
-                                <p>" +
-      param.products[i].price +
-      "</p><br><br>\
-                                <p>" +
-      param.products[i].category.name +
-      '</p>\
+                                <h2>'+param.products[i].name+'</h2>\
+                                <p class="flip-box-text">CHF '+param.products[i].price+'</p>\
+                                <img src="'+param.products[i].image+'" alt="" wide="40%" height="40%">\
                             </div>\
                             <div class="flip-box-back">\
-                                <h2>Beschreibung</h2>\
-                                <p>' +
-      param.products[i].description +
-      '</p>\
-                                <img src="' +
-      param.products[i].image +
-      '" alt="" wide="50%" height="50%">\
+                                 <h2>'+param.products[i].name+'</h2>\
+                                 <img src="'+param.products[i].image+'" alt="" wide="20%" height="20%">\
+                                 <p class="flip-box-text">'+param.products[i].description+'</p>\
+                                 <p class="flip-box-text">Kategorie: '+ param.products[i].category.name +'</p>\
+                                 <p class="flip-box-text">Artikel-Nr.: '+param.products[i].sku+'</p>\
                             </div>\
                     </div>\
                 </div>\
@@ -41,80 +27,80 @@ const APIresponse = (param) => {
   }
 };
 
+
+
+
 fetch("https://web-modules.dev/api/v1/products", {
   headers: {
-    Authorization: "Bearer 87|Li56CUyS4RmeOywFWwM3Gmq0YKw0nYvBPQygTxgx", // Part nach "Bearer " mit eigenem Token ersetzen
+      Authorization: "Bearer 87|Li56CUyS4RmeOywFWwM3Gmq0YKw0nYvBPQygTxgx", // Part nach "Bearer " mit eigenem Token ersetzen
   },
 })
   .then((result) => result.json()) // JSON aus der Antwort annehmen
   .then((json) => APIresponse(json))
   .catch(
-    (error) => console.error(error)
-    // Fehlerbehandlung
+      (error) => console.error(error)
+      // Fehlerbehandlung
   );
 
 //Testimonials
 const APITestimonials = (param) => {
   for (i = 0; i < param.testimonials.length; i++) {
-    console.log(i);
-    console.log(param.testimonials.length);
-    console.log(param.testimonials[i]);
-    console.log(param.testimonials[i].lastname);
-    console.log(param.testimonials);
-    document.getElementById("testimonials").innerHTML +=
-      '<div class="testimonial-col">\
+      console.log(i);
+      console.log(param.testimonials.length);
+      console.log(param.testimonials[i]);
+      console.log(param.testimonials[i].lastname);
+      console.log(param.testimonials);
+      document.getElementById("testimonials").innerHTML +=
+          '<div class="testimonial-col">\
              <div class="testimonial">\
                 <div class="testimonial-card">\
                     <img src="' +
-      param.testimonials[i].avatar +
-      '" alt="">\
+          param.testimonials[i].avatar +
+          '" alt="">\
                     <div class="testimonial-title"> <div class="name">' +
-      param.testimonials[i].lastname +
-      " " +
-      param.testimonials[i].firstname +
-      '</div>\
+          param.testimonials[i].lastname +
+          " " +
+          param.testimonials[i].firstname +
+          '</div>\
                     <div class="company">' +
-      param.testimonials[i].company +
-      '</div>\
+          param.testimonials[i].company +
+          '</div>\
                     </div>\
                 </div>\
                 <div class="testimonial-quote">' +
-      param.testimonials[i].quote +
-      '</div>\
+          param.testimonials[i].quote +
+          '</div>\
                 <div class="testimonial-rating">Likes: <button type="button" onClick="like(\'testimonial\',' +
-      param.testimonials[i].id +
-      ')">' +
-      param.testimonials[i].likes_count +
-      "</button>\
+          param.testimonials[i].id +
+          ')">' +
+          param.testimonials[i].likes_count +
+          '</button>\
         </div>\
             </div>\
-        </div>";
+        </div>';
   }
 };
 //get Abfrage
 fetch("https://web-modules.dev/api/v1/testimonials/10", {
   headers: {
-    Authorization: "Bearer 87|Li56CUyS4RmeOywFWwM3Gmq0YKw0nYvBPQygTxgx", // Part nach "Bearer " mit eigenem Token ersetzen
+      Authorization: "Bearer 87|Li56CUyS4RmeOywFWwM3Gmq0YKw0nYvBPQygTxgx", // Part nach "Bearer " mit eigenem Token ersetzen
   },
 })
   .then((result) => result.json()) // JSON aus der Antwort annehmen
   .then((json) => APITestimonials(json))
   .catch(
-    (error) => console.error(error)
-    // Fehlerbehandlung
+      (error) => console.error(error)
+      // Fehlerbehandlung
   );
 //Post
 const like = (type, id) =>
   console.log(
-    "https://web-modules.dev/api/v1/like?type=" + type + "&id=" + id + ""
+      "https://web-modules.dev/api/v1/like?type=" + type + "&id=" + id + ""
   );
 fetch("https://web-modules.dev/api/v1/like?type=" + type + "&id=" + id + "", {
   method: "POST",
   mode: "no-cors",
   headers: {
-    Authorization: "Bearer 87|Li56CUyS4RmeOywFWwM3Gmq0YKw0nYvBPQygTxgx", // Part nach "Bearer " mit eigenem Token ersetzen
+      Authorization: "Bearer 87|Li56CUyS4RmeOywFWwM3Gmq0YKw0nYvBPQygTxgx", // Part nach "Bearer " mit eigenem Token ersetzen
   },
-  body: "type=testimonials&id=3",
-})
-  .then((result) => result.json())
-  .then((json) => console.log(json));
+});
